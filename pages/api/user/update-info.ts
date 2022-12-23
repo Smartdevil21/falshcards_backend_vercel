f6826b69d600ab0e33098c4251519b629c7ace34
pratik1803 <pratikvaidya1803@gmail.com>
2022-12-23 18:20:55 +0530

    added update user info and send recovery email apis

---
+++ pages/api/user/update-info.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { User } from "../../../server/models/user.model";
+import * as bcrypt from "bcryptjs";
+
+export default async function updateInfo(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { method } = req;
+    if (method === "OPTIONS") {
+      return res.status(200).send("ok");
+    }
+    let user = await User.findOne({ _id: req.body.uid });
+    user.password = await bcrypt.hash(req.body.updatedPass, 10);
+    const result = await User.findOneAndUpdate({ _id: req.body.uid }, user);
+    res.status(200).json({
+      succes: true,
+      data: result,
+    });
+  } catch (error) {
+    console.log(`${error}`);
+    res
+      .status(400)
+      .json({ success: false, data: { message: "Something went wrong!" } });
+  }
+}
